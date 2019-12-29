0215b12c2f105666412e0d7985561338604dea3d
Akilesh Sharma <30718139+akileshsharma18@users.noreply.github.com>
2019-12-29 10:55:09 +0530

    Add files via upload

---
+++ new 2.s
@@ -0,0 +1,141 @@
+mov r0,#7
+mov r1,#-25
+mov r2,#-40
+mov r3,#5
+mov r4,#15
+mov r5,#25
+
+cmp r0,r1
+
+movgt r9,r0
+movgt r0,r1
+movgt r1,r9
+
+movlt r0,r0
+movlt r1,r1
+
+cmp r1,r2
+
+movgt r9,r1
+movgt r1,r2
+movgt r2,r9
+
+movlt r1,r1
+movlt r2,r2
+
+
+cmp r2,r3
+
+movgt r9,r2
+movgt r2,r3
+movgt r3,r9
+
+movlt r2,r2
+movlt r3,r3
+
+cmp r3,r4
+
+movgt r9,r3
+movgt r3,r4
+movgt r4,r9
+
+movlt r3,r3
+movlt r4,r4
+
+cmp r4,r5
+
+movgt r9,r4
+movgt r4,r5
+movgt r5,r9
+
+movlt r4,r4
+movlt r5,r5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cmp r0,r1
+
+movgt r9,r0
+movgt r0,r1
+movgt r1,r9
+
+movlt r0,r0
+movlt r1,r1
+
+cmp r1,r2
+
+movgt r9,r1
+movgt r1,r2
+movgt r2,r9
+
+movlt r1,r1
+movlt r2,r2
+
+
+cmp r2,r3
+
+movgt r9,r2
+movgt r2,r3
+movgt r3,r9
+
+movlt r2,r2
+movlt r3,r3
+
+cmp r3,r4
+
+movgt r9,r3
+movgt r3,r4
+movgt r4,r9
+
+movlt r3,r3
+movlt r4,r4
+
+cmp r4,r5
+
+movgt r9,r4
+movgt r4,r5
+movgt r5,r9
+
+movlt r4,r4
+movlt r5,r5
+
+
+
+
+
+
+
+
+
+
+
